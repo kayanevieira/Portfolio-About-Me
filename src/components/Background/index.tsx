d988ba15ae3a440a8c2fc8785136c2e8a6af9ab5
Kayane Vieira <76007614+kayanevieira@users.noreply.github.com>
2022-02-04 13:13:05 -0300

    initial commit

---
+++ src/components/Background/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from './Styles.module.scss';
+
+export const Background = () => {
+    return(
+        <div className={styles.background}>
+            
+        </div>
+    )
+}
---
+++ src/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import { Background } from './components/Background';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Background />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
